ea4d5189005b75c5fe9765e446e980aba4674f7a
Christophe Labonne <labonne@asahinet.com>
2018-08-10 15:16:47 +0900

    Implemented a basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use std::env;
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
 #[no_mangle]
 pub extern "C" fn add_one(x: i32) -> i32 {
     x + 1
 }
 
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    let path = env::current_dir()?;
+    println!("cwd = {:?}", path.display());
+    println!("Path = {:?}", Path::new("./websrc").as_os_str());
+    NamedFile::open("./websrc/index.html")
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("websrc/").join(file)).ok()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, files])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
