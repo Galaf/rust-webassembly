c07365fd99aa136e91f182e52c558ef72aa88750
Christophe Labonne <galaf77@gmail.com>
2018-08-11 21:35:01 +0900

    Only execute next command if the current one succeeds

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,3 +1,1 @@
-rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/web.rs -o websrc/wa/rwa.big.wasm
-wasm-gc websrc/wa/rwa.big.wasm websrc/wa/rwa.min.wasm
-rm websrc/wa/rwa.big.wasm
+rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/web.rs -o websrc/wa/rwa.big.wasm && wasm-gc websrc/wa/rwa.big.wasm websrc/wa/rwa.min.wasm && rm websrc/wa/rwa.big.wasm
