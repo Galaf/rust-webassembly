52557190957ce1a6d12c6e59c58bc174d7f43d21
Christophe Labonne <galaf77@gmail.com>
2018-08-11 22:55:16 +0900

    Non functional, but trying to load data inside the canvas

--- websrc/js/bundle.js
+++ websrc/js/bundle.js
@@ -1,4 +1,26 @@
 WebAssembly.instantiateStreaming(fetch("/wa/rwa.min.wasm")).then(results => {
+  let alloc = results.instance.exports.alloc;
+  let buffer = results.instance.exports.memory.buffer;
+  let dealloc = results.instance.exports.dealloc;
   let fill = results.instance.exports.fill;
-  console.log(fill());
+
+  let width = 600;
+  let height = 600;
+
+  let canvas = document.getElementById("screen");
+
+  if (canvas.getContext) {
+    let ctx = canvas.getContext("2d");
+
+    let byteSize = width * height * 4;
+    console.log(byteSize);
+    let pointer = alloc(byteSize);
+    console.log(pointer);
+
+    let usub = new Uint8ClampedArray(buffer, pointer, byteSize);
+    let img = new ImageData(usub, width, height);
+
+    fill(pointer, width, height);
+    ctx.putImageData(img, 0, 0);
+  }
 });
