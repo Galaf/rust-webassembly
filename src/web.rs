469dac623e711d796d0e9d7b16df461efab0bff6
Christophe Labonne <galaf77@gmail.com>
2018-08-11 19:15:06 +0900

    Added allocation and deallocation functions

--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,21 @@
+use std::mem;
+use std::os::raw::c_void;
+
+#[no_mangle]
+pub extern "C" fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc(ptr: *mut c_void, capacity: usize) {
+    unsafe {
+        let _buf = Vec::from_raw_parts(ptr, 0, capacity);
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn add_one(x: i32) -> i32 {
     x + 1
