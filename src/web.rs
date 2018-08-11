506e49a9dcc04b8996489fb2becbb6354a804187
Christophe Labonne <galaf77@gmail.com>
2018-08-11 21:36:31 +0900

    Basic mandelbrot. Not functional yet

--- src/web.rs
+++ src/web.rs
@@ -2,6 +2,8 @@ use std::ffi::CString;
 use std::mem;
 use std::os::raw::{c_char, c_void};
 
+mod mandelbrot;
+
 #[no_mangle]
 pub extern "C" fn alloc(size: usize) -> *mut c_void {
     let mut buf = Vec::with_capacity(size);
@@ -18,6 +20,9 @@ pub extern "C" fn dealloc(ptr: *mut c_void, capacity: usize) {
 }
 
 #[no_mangle]
-pub extern "C" fn fill() -> *mut c_char {
-    CString::new("TODO").unwrap().into_raw()
+pub extern "C" fn fill(pointer: *mut u8, max_width: usize, max_height: usize) -> *mut c_char {
+    let canvas = mandelbrot::draw(max_width, max_height);
+    let s = format!("{?}", canvas);
+
+    return CString::new(s).unwrap().into_raw();
 }
