9c1a82cfb3c2c1164e33ff1efa4c018d10e3066e
Christophe Labonne <galaf77@gmail.com>
2018-08-12 14:57:53 +0900

    The buffer is now correctly used

--- websrc/js/bundle.js
+++ websrc/js/bundle.js
@@ -1,26 +1,37 @@
-WebAssembly.instantiateStreaming(fetch("/wa/rwa.min.wasm")).then(results => {
-  let alloc = results.instance.exports.alloc;
-  let buffer = results.instance.exports.memory.buffer;
-  let dealloc = results.instance.exports.dealloc;
-  let fill = results.instance.exports.fill;
+fetch("/wa/rwa.min.wasm")
+  .then(reponse => reponse.arrayBuffer())
+  .then(bytes => WebAssembly.instantiate(bytes))
+  .then(results => {
+    let alloc = results.instance.exports.alloc;
+    let dealloc = results.instance.exports.dealloc;
+    let fill = results.instance.exports.fill;
 
-  let width = 600;
-  let height = 600;
-
-  let canvas = document.getElementById("screen");
-
-  if (canvas.getContext) {
-    let ctx = canvas.getContext("2d");
+    let width = 600;
+    let height = 600;
 
     let byteSize = width * height * 4;
-    console.log(byteSize);
     let pointer = alloc(byteSize);
-    console.log(pointer);
+    let buffer = new Uint8Array(
+      results.instance.exports.memory.buffer,
+      pointer,
+      byteSize,
+    );
 
-    let usub = new Uint8ClampedArray(buffer, pointer, byteSize);
-    let img = new ImageData(usub, width, height);
+    let canvas = document.getElementById("screen");
 
-    fill(pointer, width, height);
-    ctx.putImageData(img, 0, 0);
-  }
-});
+    if (canvas.getContext) {
+      let ctx = canvas.getContext("2d");
+
+      let pointer = alloc(byteSize);
+
+      let usub = new Uint8ClampedArray(
+        results.instance.exports.memory.buffer,
+        pointer,
+        byteSize,
+      );
+      let img = new ImageData(usub, width, height);
+
+      fill(pointer, width, height);
+      ctx.putImageData(img, 0, 0);
+    }
+  });
