5b87eaa6fec956df6f43c0497bf29c864152b899
Christophe Labonne <labonne@asahinet.com>
2018-08-17 16:01:39 +0900

    Disabled basic-http-server for now

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cargo +nightly build --target wasm32-unknown-unknown
 
 wasm-bindgen target/wasm32-unknown-unknown/debug/rust_webassembly.wasm --no-modules --out-dir ./static/wa/
 
-basic-http-server ./static/
+#basic-http-server ./static/
