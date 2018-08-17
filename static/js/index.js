d906011c0a95cc80509e3dea6d3e385777d33e3d
Christophe Labonne <labonne@asahinet.com>
2018-08-17 16:02:46 +0900

    Added option to customize the set from the browser

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,25 @@
 const { draw } = wasm_bindgen;
 
+function getSubmitData() {
+    return {
+        iterations: parseInt(document.getElementById("iterations").value, 10)
+    };
+}
+
 function run() {
   const canvas = document.getElementById("screen");
   const ctx = canvas.getContext("2d");
+
+  const formConf = getSubmitData();
+
   const configuration = {
-    iterations: 1000,
+    iterations: formConf.iterations,
     width: canvas.width,
     height: canvas.height,
   };
   draw(ctx, configuration);
 }
 
-wasm_bindgen("../wa/rust_webassembly_bg.wasm").then(run);
+(function () {
+    wasm_bindgen("../wa/rust_webassembly_bg.wasm").then(run);
+})();
